e3f1f374a1eb689daf6b315896d6c67a7d9f31ac
Kevin Wang <kevmo314@gmail.com>
2023-04-28 20:33:29 -0400

    fix: correct imports

--- src/find.ts
+++ src/find.ts
@@ -1,26 +1,26 @@
-import { decode } from 'geobuf'
-import inside from '@turf/boolean-point-in-polygon'
-import { point } from '@turf/helpers'
-import Pbf from 'pbf'
+import { decode } from "geobuf";
+import inside from "@turf/boolean-point-in-polygon";
+import { point } from "@turf/helpers";
+import Pbf from "pbf";
 
-import { getTimezoneAtSea, oceanZones } from './oceanUtils'
+import { getTimezoneAtSea, oceanZones } from "./oceanUtils";
 
 async function geoData() {
   const response = await fetch(
-    'https://www.unpkg.com/geo-tz@latest/data/geo.dat'
-  )
-  return await response.arrayBuffer()
+    "https://www.unpkg.com/geo-tz@latest/data/geo.dat"
+  );
+  return await response.arrayBuffer();
 }
 
 async function tzData() {
   const response = await fetch(
-    'https://www.unpkg.com/geo-tz@latest/data/index.json'
-  )
-  return await response.json()
+    "https://www.unpkg.com/geo-tz@latest/data/index.json"
+  );
+  return await response.json();
 }
 
-let geoDataPromise: Promise<any> | null = null
-let tzDataPromise: Promise<any> | null = null
+let geoDataPromise: Promise<any> | null = null;
+let tzDataPromise: Promise<any> | null = null;
 
 /**
  * Find the timezone ID(s) at the given GPS coordinates.
@@ -30,41 +30,41 @@ let tzDataPromise: Promise<any> | null = null
  * @returns An array of string of TZIDs at the given coordinate.
  */
 export async function find(lat: number, lon: number): Promise<string[]> {
-  const originalLon = lon
+  const originalLon = lon;
 
-  let err
+  let err;
 
   // validate latitude
   if (isNaN(lat) || lat > 90 || lat < -90) {
-    err = new Error('Invalid latitude: ' + lat)
-    throw err
+    err = new Error("Invalid latitude: " + lat);
+    throw err;
   }
 
   // validate longitude
   if (isNaN(lon) || lon > 180 || lon < -180) {
-    err = new Error('Invalid longitude: ' + lon)
-    throw err
+    err = new Error("Invalid longitude: " + lon);
+    throw err;
   }
 
   // North Pole should return all ocean zones
   if (lat === 90) {
-    return oceanZones.map((zone) => zone.tzid)
+    return oceanZones.map((zone) => zone.tzid);
   }
 
   // fix edges of the world
   if (lat >= 89.9999) {
-    lat = 89.9999
+    lat = 89.9999;
   } else if (lat <= -89.9999) {
-    lat = -89.9999
+    lat = -89.9999;
   }
 
   if (lon >= 179.9999) {
-    lon = 179.9999
+    lon = 179.9999;
   } else if (lon <= -179.9999) {
-    lon = -179.9999
+    lon = -179.9999;
   }
 
-  const pt = point([lon, lat])
+  const pt = point([lon, lat]);
 
   // get exact boundaries
   const quadData = {
@@ -74,66 +74,66 @@ export async function find(lat: number, lon: number): Promise<string[]> {
     right: 179.9999,
     midLat: 0,
     midLon: 0,
-  }
-  let quadPos = ''
+  };
+  let quadPos = "";
   if (!tzDataPromise) {
-    tzDataPromise = tzData()
+    tzDataPromise = tzData();
   }
 
-  let curTzData = (await tzDataPromise).lookup
+  let curTzData = (await tzDataPromise).lookup;
 
   while (true) {
     // calculate next quadtree position
-    let nextQuad
+    let nextQuad;
     if (lat >= quadData.midLat && lon >= quadData.midLon) {
-      nextQuad = 'a'
-      quadData.bottom = quadData.midLat
-      quadData.left = quadData.midLon
+      nextQuad = "a";
+      quadData.bottom = quadData.midLat;
+      quadData.left = quadData.midLon;
     } else if (lat >= quadData.midLat && lon < quadData.midLon) {
-      nextQuad = 'b'
-      quadData.bottom = quadData.midLat
-      quadData.right = quadData.midLon
+      nextQuad = "b";
+      quadData.bottom = quadData.midLat;
+      quadData.right = quadData.midLon;
     } else if (lat < quadData.midLat && lon < quadData.midLon) {
-      nextQuad = 'c'
-      quadData.top = quadData.midLat
-      quadData.right = quadData.midLon
+      nextQuad = "c";
+      quadData.top = quadData.midLat;
+      quadData.right = quadData.midLon;
     } else {
-      nextQuad = 'd'
-      quadData.top = quadData.midLat
-      quadData.left = quadData.midLon
+      nextQuad = "d";
+      quadData.top = quadData.midLat;
+      quadData.left = quadData.midLon;
     }
 
     // console.log(nextQuad)
-    curTzData = curTzData[nextQuad]
+    curTzData = curTzData[nextQuad];
     // console.log()
-    quadPos += nextQuad
+    quadPos += nextQuad;
 
     // analyze result of current depth
     if (!curTzData) {
       // no timezone in this quad, therefore must be timezone at sea
-      return getTimezoneAtSea(originalLon)
+      return getTimezoneAtSea(originalLon);
     } else if (curTzData.pos >= 0 && curTzData.len) {
       // get exact boundaries
       if (!geoDataPromise) {
-        geoDataPromise = geoData()
+        geoDataPromise = geoData();
       }
       const bufSlice = (await geoDataPromise).slice(
         curTzData.pos,
         curTzData.pos + curTzData.len
-      )
-      const geoJson = decode(new Pbf(bufSlice))
+      );
+      const geoJson = decode(new Pbf(bufSlice));
 
-      const timezonesContainingPoint = []
+      const timezonesContainingPoint = [];
 
-      if (geoJson.type === 'FeatureCollection') {
+      if (geoJson.type === "FeatureCollection") {
         for (let i = 0; i < geoJson.features.length; i++) {
           if (inside(pt, geoJson.features[i] as any)) {
-            timezonesContainingPoint.push(geoJson.features[i].properties.tzid)
+            timezonesContainingPoint.push(geoJson.features[i].properties.tzid);
           }
         }
-      } else if (geoJson.type === 'Feature') {
+      } else if (geoJson.type === "Feature") {
         if (inside(pt, geoJson as any)) {
-          timezonesContainingPoint.push(geoJson.properties.tzid)
+          timezonesContainingPoint.push(geoJson.properties.tzid);
         }
       }
 
@@ -141,26 +141,26 @@ export async function find(lat: number, lon: number): Promise<string[]> {
       // otherwise must be timezone at sea
       return timezonesContainingPoint.length > 0
         ? timezonesContainingPoint
-        : getTimezoneAtSea(originalLon)
+        : getTimezoneAtSea(originalLon);
     } else if (curTzData.length > 0) {
       // exact match found
-      const timezones = (await tzDataPromise).timezones
-      return curTzData.map((idx) => timezones[idx])
-    } else if (typeof curTzData !== 'object') {
+      const timezones = (await tzDataPromise).timezones;
+      return curTzData.map((idx) => timezones[idx]);
+    } else if (typeof curTzData !== "object") {
       // not another nested quad index, throw error
-      err = new Error('Unexpected data type')
-      throw err
+      err = new Error("Unexpected data type");
+      throw err;
     }
 
     // calculate next quadtree depth data
-    quadData.midLat = (quadData.top + quadData.bottom) / 2
-    quadData.midLon = (quadData.left + quadData.right) / 2
+    quadData.midLat = (quadData.top + quadData.bottom) / 2;
+    quadData.midLon = (quadData.left + quadData.right) / 2;
   }
 }
 
 export function toOffset(timeZone: string) {
-  const date = new Date()
-  const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }))
-  const tzDate = new Date(date.toLocaleString('en-US', { timeZone }))
-  return (tzDate.getTime() - utcDate.getTime()) / 6e4
+  const date = new Date();
+  const utcDate = new Date(date.toLocaleString("en-US", { timeZone: "UTC" }));
+  const tzDate = new Date(date.toLocaleString("en-US", { timeZone }));
+  return (tzDate.getTime() - utcDate.getTime()) / 6e4;
 }
